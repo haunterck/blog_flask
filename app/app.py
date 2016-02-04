e3d7fb9b42e5ebb0fc4d5b735ef33f580d2e7128
Mario <mcoahuila@mimoni.com>
2016-02-04 16:17:04 -0600

    Add manage script to do migrations; status column in Entry model

--- app/app.py
+++ app/app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask.ext.migrate import Migrate, MigrateCommand
+from flask.ext.script import Manager
 from flask.ext.sqlalchemy import SQLAlchemy
 # Imports our configuration data
 from config import Configuration
@@ -7,3 +9,7 @@ app = Flask(__name__)
 # Use values from Configuration class
 app.config.from_object(Configuration)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
--- app/models.py
+++ app/models.py
@@ -4,14 +4,33 @@ from app import db
 def slugify(s):
     return re.sub('[^\w]+', '-', s).lower()
 
+# describes the relationship between Entry & Tag tables
+entry_tags = db.Table('entry_tags',
+    db.Column( 'tag_id', db.Integer, db.ForeignKey('tag.id') ),
+    db.Column( 'entry_id', db.Integer, db.ForeignKey('entry.id') )
+)
+
 class Entry(db.Model):
+    '''
+    Model that represents a blog entry, defining its
+    attributes
+    '''
+    STATUS_PUBLIC = 0
+    STATUS_DRAFT = 1
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     slug = db.Column(db.String(100), unique=True)
     body = db.Column(db.Text)
+    status = db.Column(db.SmallInteger, default=STATUS_PUBLIC)
     created_timestamp = db.Column(db.DateTime, default=datetime.datetime.now)
     modified_timestamp = db.Column(db.DateTime, default=datetime.datetime.now,
                          onupdate=datetime.datetime.now)
+
+    # Specifies the relationship with Tag entity(table)
+    tags = db.relationship('Tag', secondary=entry_tags,
+        backref=db.backref('entries', lazy='dynamic')
+    )
 
     def __init__(self, *args, **kwargs):
         # Call parent constructor
@@ -21,7 +40,23 @@ class Entry(db.Model):
     def generate_slug(self):
         self.slug = ''
         if self.title:
-            self.slug = slugify(sefl.title)
+            self.slug = slugify(self.title)
 
     def __repr__(self):
         return '<Entry: %s>' % self.title
+
+class Tag(db.Model):
+    '''
+    Model that contains the tags used inside blog entries
+    to be identified
+    '''
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    slug = db.Column(db.String(64), unique=True)
+
+    def __init__(self, *args, **kwargs):
+        super(Tag, self).__init__(*args, **kwargs)
+        self.slug = slugify(self.name)
+
+    def __repr__(self):
+        return '<Tag %s>' % self.name
